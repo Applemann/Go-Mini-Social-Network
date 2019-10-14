c369992fad18e982c5adb74246f49b3d28a6905b
Martin Jablecnik <martin.jablecnik@email.cz>
2019-10-14 20:56:23 +0200

    Update .gitignore and add startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export APP_PORT=5000
+export APP_SESSION_SECRET=fjiwo4r349t
+export DB_ADDRESS=localhost
+export DB_PORT=3307
+export DB_USER=root
+export DB_PASSWORD=test1
+export DB=social_network
+
+./main
